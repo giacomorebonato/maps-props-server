99f7bc5a859fb0c18a1f9f0c1718849e94400cec
Giacomo Rebonato <grebonato@hubspot.com>
2018-08-22 13:25:58 +0100

    Started scraper

--- README.md
+++ README.md
@@ -4,3 +4,15 @@
 
 This is the API backend for storing the landmarks for
 the maps-props frontend.
+
+## Objective
+
+Scrape data in Dublin area relevant to house costs and IT jobs.
+
+## How to work on this?
+
+### Migrations
+
+### End points
+
+### Queries
--- src/scrapers/scrapeDaft.js
+++ src/scrapers/scrapeDaft.js
@@ -1,10 +1,24 @@
 let puppeteer = require('puppeteer')
 
+function parseFloorArea(floorAreaText) {}
+
 async function scrapeDaft(request, reply) {
   let browser = await puppeteer.launch()
   let page = await browser.newPage()
 
+  await page.setViewport({ width: 1920, height: 1080 })
+
   page.goto(request.body.url)
+
+  let addressNode = page.$('#smi-map-link')
+  let floorNodes = document.querySelectorAll('.description_block')
+
+  floorNodes.forEach(block => {
+    if (block.textContent.includes('Floor Area')) {
+    }
+  })
+
+  browser.close()
 
   return {}
 }
