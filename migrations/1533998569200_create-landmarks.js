6730c088b3394e5a05f8db8a98cd83172a92b45d
Giacomo Rebonato <grebonato@hubspot.com>
2018-08-22 18:38:07 +0100

    Added migrations for dafts table

--- migrations/1533998569200_create-landmarks.js
+++ migrations/1533998569200_create-landmarks.js
@@ -3,10 +3,8 @@ exports.shorthands = undefined
 const LANDMARKS_TABLE = 'landmarks'
 
 exports.up = pgm => {
-  pgm.createType('category', ['apartment', 'company'])
   pgm.createTable(LANDMARKS_TABLE, {
     id: 'id',
-    category: { type: 'category', notNull: true },
     location: { type: 'GEOGRAPHY(Point)', notNull: true },
     name: { type: 'varchar(1000)', notNull: true },
     createdAt: {
